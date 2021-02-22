2be46b715d5e3fd940e1aa2c1f173fa2f69039ce
Ilia Muravev <degtyarev327@gmail.com>
2021-02-22 11:51:12 +0300

    add uniq package with tests

---
+++ uniq/uniq/uniq.go
@@ -0,0 +1,82 @@
+package uniq
+
+import (
+	"fmt"
+	"strings"
+)
+
+type RunOptions struct {
+	Count      *bool
+	Duplicates *bool
+	Unique     *bool
+	SkipFields *int
+	SkipChars  *int
+	IgnoreCase *bool
+}
+
+func getModifiedLine(line string, options RunOptions) string {
+	if *options.IgnoreCase {
+		line = strings.ToUpper(line)
+	}
+
+	if *options.SkipChars > 0 {
+		if len(line) >= *options.SkipChars {
+			line = line[*options.SkipChars:]
+		}
+	}
+
+	if *options.SkipFields > 0 {
+		splited := strings.Split(line, " ")
+		if len(splited) > 1 {
+			line = strings.Join(splited[*options.SkipFields:], " ")
+		}
+	}
+
+	return line
+}
+
+func Uniq(lines []string, options RunOptions) []string {
+	linesMap := make(map[string][]int)
+	linesPositions := make([]int, 0)
+
+	for index, line := range lines {
+		line = getModifiedLine(line, options)
+
+		if _, has := linesMap[line]; !has {
+			linesMap[line] = make([]int, 0)
+			linesPositions = append(linesPositions, index)
+		}
+
+		linesMap[line] = append(linesMap[line], index)
+	}
+
+	resultLines := make([]string, 0)
+
+	if *options.Count {
+		for _, pos := range linesPositions {
+			line := lines[pos]
+
+			if count := len(linesMap[getModifiedLine(line, options)]); count > 0 {
+				resultLines = append(resultLines, fmt.Sprint(count)+" "+line)
+			}
+		}
+	} else if *options.Duplicates {
+		for _, pos := range linesPositions {
+			if line := lines[pos]; len(linesMap[getModifiedLine(line, options)]) > 1 {
+				resultLines = append(resultLines, line)
+			}
+		}
+	} else if *options.Unique {
+		for _, pos := range linesPositions {
+			if line := lines[pos]; len(linesMap[getModifiedLine(line, options)]) == 1 {
+				resultLines = append(resultLines, line)
+			}
+		}
+	} else {
+		for _, pos := range linesPositions {
+			resultLines = append(resultLines, lines[pos])
+		}
+	}
+
+	return resultLines
+}
